a46c1b98749584010ceb57df0c543217de47855c
bruceboelens <bruceboelens@outlook.com>
2019-07-24 10:06:58 +0200

    created the inactive screen and implemented it

---
+++ resources/js/components/InactiveScreen.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div class="row justify-content-center center-row">
+            <div class= "col-md-5 " id="blue-man-flex">
+                <img src="images/inactive/blue-man.svg" alt="blue-man"/>
+            </div>
+            <div class="col-md-7 align-items-center justify-content-center inactive-lottie ">
+                    <h1 id="step-on">Step on &</h1>
+                    <div class="lottie-box">
+                        <lottie :options="defaultOptions" id="inactive"/>
+                    </div>
+            </div>
+        </div>
+        <div>
+            <img src="images/inactive/bottom-line.svg" alt="bottom-line" id="bottom-line"/>
+        </div>
+    </div>
+</template>
+<script>
+import {router} from '../app.js';
+import lottie from 'lottie-web';
+import Lottie from 'vue-lottie';
+import animationData from '../lottie/data.json';
+import Pusher from "../../../public/js/pusher/index.js";
+
+
+export default {
+    name: 'InactiveScreen',
+    components: {
+        Lottie
+    },
+    data: function() {
+        return {
+        activeIndex: 0,
+        defaultOptions: { animationData: animationData, loop: true},
+        }
+    },
+    mounted() {
+        this.startInstructions();
+    },
+
+    methods: {
+        startInstructions: function(){
+            let channel = Pusher.subscribe('particle-channel');
+            channel.bind('particle-data', (data) => {
+                router.push({
+                    name: 'instruction'
+                });
+            });
+      }
+  }
+}
+</script>
+<style>
+#blue-man-flex {
+    align-self: flex-end;
+}
+
+#bottom-line {
+    position: absolute;
+    bottom: 0;
+}
+
+#step-on {
+    margin-bottom: 0;
+    color: #212121;
+    font-size: 5rem;
+}
+
+#inactive {
+    width: 0%;
+    height: 0%;
+}
+
+.lottie-box {
+    width: 77%;
+}
+
+.inactive-lottie {
+    margin-top: 17rem;
+    width: 50%;
+}
+
+
+
+</style>
